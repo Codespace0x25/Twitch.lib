1aca6ca105f110911907623af695970ae211f23e
mineraft teen <trevorleecox@icloud.com>
2024-04-03 15:09:59 -0500

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+
+namespace ConsoleApp4
+{
+    internal class Chat
+    {
+        private TcpListener _listener;
+        public Chat(string displayname) {
+            _listener = new TcpListener(IPAddress.Parse()
+        }
+        public void sendChat(string url)
+        {
+
+
+             
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Chat chat = new Chat();
+            chat.sendChat("https://google.com");
+            chat
+        }
+        
+    }
+ 
+}
